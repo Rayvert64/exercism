c6f45981ea33d5a69ee6708bcc3cdef7d518a72f
Adora Boutin <philippe.boutin@protonmail.com>
2023-05-20 21:09:37 -0400

    No need for reference counting in scoped threads

--- rust/buffer-play/src/main.rs
+++ rust/buffer-play/src/main.rs
@@ -1,32 +1,26 @@
 use num_cpus;
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-};
+use std::{slice::ChunksMut, thread};
 
 fn main() {
     // Create a buffer
-    let mut buffer = vec![0u64; 10240000];
+    let mut buffer = vec![0u64; 1024000000];
     let buffer_len = buffer.len();
 
     let num_tasks = num_cpus::get();
 
     let chunk_size = buffer_len / num_tasks;
 
-    let mut chunked_data = vec![];
-    buffer
-        .chunks_mut(chunk_size)
-        .for_each(|slice| chunked_data.push(Arc::new(Mutex::new(slice))));
+    let chunked_data = buffer.chunks_mut(chunk_size);
+
     multi_write_buffers(chunked_data, chunk_size);
 
-    println!("Buffer: {:?}", buffer);
+    //println!("Buffer: {:?}", buffer);
 }
 
-pub fn multi_write_buffers<'a>(buffers: Vec<Arc<Mutex<&'a mut [u64]>>>, chunk_size: usize) {
+pub fn multi_write_buffers(buffers: ChunksMut<u64>, chunk_size: usize) {
     thread::scope(|t| {
         for (i, buffer) in buffers.into_iter().enumerate() {
             t.spawn(move || {
-                let mut buffer = buffer.lock().unwrap();
                 for j in 0..buffer.len() {
                     buffer[j] = ((i * chunk_size) + j) as u64;
                 }
