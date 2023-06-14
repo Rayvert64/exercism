080be6169e14853b51341a4dada7fc2ff6524d8c
Adora Boutin <philippe.boutin@protonmail.com>
2023-06-14 16:10:37 -0400

    Submited gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use time::PrimitiveDateTime as DateTime;
 
+const GIGASECOND: time::Duration = time::Duration::seconds(1000000000);
+
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: &DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    start.clone().saturating_add(GIGASECOND)
 }
